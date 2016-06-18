1f3e0ee982d0d0e21eb88409be9e2c4c1dd31452
Saigesp <saigesp@saigesp.es>
2016-06-18 14:34:21 +0200

    update page examples

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,67 +1,47 @@
-  function ToggleMenu(args){
-      if(jQuery('#'+args).hasClass('active') || args == 'close'){
-        jQuery('.js-menu').removeClass("active");
-        jQuery('#overlaybody').removeClass("active");
-      }else{
-        jQuery('.js-menu').removeClass("active");
-        jQuery('#overlaybody').addClass("active");
-        jQuery('#'+args).addClass("active");
-      }
+function ToggleMenu(args){
+    if(jQuery('#'+args).hasClass('active') || args == 'close'){
+      jQuery('.js-menu').removeClass("active");
+      jQuery('#overlaybody').removeClass("active");
+    }else{
+      jQuery('.js-menu').removeClass("active");
+      jQuery('#overlaybody').addClass("active");
+      jQuery('#'+args).addClass("active");
     }
+  }
 
-    function ToggleSection(elem){
-      var elem = jQuery(elem)[0];
-      var section = jQuery(elem).data('section');
+function ToggleSection(elem){
+  var elem = jQuery(elem)[0];
+  var section = jQuery(elem).data('section');
 
-      if(jQuery(elem).hasClass('active') || section == 'close'){
-        jQuery(elem).removeClass('active');
-        jQuery('.js-section').removeClass("active");
-        jQuery('.js-section-launch').removeClass("active");
-      }else{
-        jQuery('.js-section').removeClass("active");
-        jQuery('.js-section-launch').removeClass("active");
-        jQuery('.js-section-'+section).addClass("active");
-        jQuery('#'+section).addClass("active");
-        jQuery(elem).addClass('active');
-      } 
-    }
+  if(jQuery(elem).hasClass('active') || section == 'close'){
+    jQuery(elem).removeClass('active');
+    jQuery('.js-section').removeClass("active");
+    jQuery('.js-section-launch').removeClass("active");
+  }else{
+    jQuery('.js-section').removeClass("active");
+    jQuery('.js-section-launch').removeClass("active");
+    jQuery('.js-section-'+section).addClass("active");
+    jQuery('#'+section).addClass("active");
+    jQuery(elem).addClass('active');
+  } 
+}
 
-    function ToggleSelect(select_id){
-      var section = jQuery('#'+select_id+' option:selected').val();
-      if(jQuery('#js-select-'+section).hasClass('active') || select_id == 'close'){
-        jQuery('.js-select-'+select_id).removeClass("active");
-      }else{
-        jQuery('.js-select-'+select_id).removeClass("active");
-        jQuery('#js-select-'+section).addClass("active");
-      }
-    }
+function ToggleSelect(select_id){
+  var section = jQuery('#'+select_id+' option:selected').val();
+  if(jQuery('#js-select-'+section).hasClass('active') || select_id == 'close'){
+    jQuery('.js-select-'+select_id).removeClass("active");
+  }else{
+    jQuery('.js-select-'+select_id).removeClass("active");
+    jQuery('#js-select-'+section).addClass("active");
+  }
+}
 
-    function imageresize() {
-        if (jQuery('.js-fullheight').length > 0) jQuery('.js-fullheight').css("height", jQuery(window).height() - jQuery('#headertop').outerHeight());
-        if (jQuery('.js-fullheight-thumb').length > 0) jQuery('.js-fullheight-thumb').imagefill();
-    }
+function imageresize() {
+    if (jQuery('.js-fullheight').length > 0) jQuery('.js-fullheight').css("height", jQuery(window).height() - jQuery('#headertop').outerHeight());
+    if (jQuery('.js-fullheight-thumb').length > 0) jQuery('.js-fullheight-thumb').imagefill();
+}
 
 
-      jQuery(document).ready(function($) {
-        $('.add_to_cart_button').on("click", function(){
+jQuery(document).ready(function($) {
 
-          var text_original = $(this).text();
-          var text_temporal = "¡Añadido!";
-          $(this).text(text_temporal);
-
-          setTimeout(function(){
-            $('.add_to_cart_button').text(text_original);
-          }, 3000);
-
-        });
-      });
-
-      jQuery(window).load(function() { 
-
-        jQuery('.js-imagefill').imagefill(); 
-        
-        jQuery('.js-slider').flickity({
-          initialIndex: 2,
-          wrapAround: true
-        });
-      });
+});
